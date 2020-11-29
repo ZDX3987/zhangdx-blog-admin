5588de3867b77d4ae00857fcfe12d7ca546f73af
zhangdx <zdx3987@163.com>
2020-11-29 13:59:58 +0800

    设置post请求请求头为json，适配苹果系统浏览器

--- src/api/user.js
+++ src/api/user.js
@@ -1,28 +1,32 @@
 import http from '@/http/index'
 
 const userapi = {
-    login(params) {
-        return http.post('/api/login', params);
-    },
+  login(params) {
+    return http.post('/api/login', params, {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8'
+      }
+    });
+  },
 
-    regist(params) {
-        return http.post('/api/regist', params);
-    },
-    logout() {
-        return http.get ('/api/logout');
-    },
-    getCurrUser() {
-        return http.get('/api/user/curruser');
-    },
-    updateUserInfo(userInfo) {
-        return http.post('/api/user/user', userInfo);
-    },
-    uploadAvatar(file) {
-        return http.post('/api/user/avatar', file, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        });
-    }
+  regist(params) {
+    return http.post('/api/regist', params);
+  },
+  logout() {
+    return http.get('/api/logout');
+  },
+  getCurrUser() {
+    return http.get('/api/user/curruser');
+  },
+  updateUserInfo(userInfo) {
+    return http.post('/api/user/user', userInfo);
+  },
+  uploadAvatar(file) {
+    return http.post('/api/user/avatar', file, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+  }
 }
 export default userapi
