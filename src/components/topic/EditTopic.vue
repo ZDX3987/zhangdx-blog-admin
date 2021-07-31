25ff1604885ec2c2153dfbe8fcd40b08c54f2724
zhangdx <zdx3987@163.com>
2021-07-31 09:25:48 +0800

    修改主题明细页面bug

--- src/components/topic/EditTopic.vue
+++ src/components/topic/EditTopic.vue
@@ -22,7 +22,7 @@
             <el-table-column prop="title" label="标题" width="300" :show-overflow-tooltip="true"></el-table-column>
             <el-table-column prop="author.username" label="作者" width="120" align="center"></el-table-column>
             <el-table-column label="标签" width="200" align="center" :show-overflow-tooltip="true">
-              <template slot-scope="scope">
+              <template v-if="scope.row" slot-scope="scope">
             <span v-for="(tag, index) of scope.row.categories" :key="tag.id">
               {{ index !== 0 ? '/' : '' + tag.cateName }}
             </span>
